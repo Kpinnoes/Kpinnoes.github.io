886d0ac75bc24c4d47d4603575d0a468633627ec
Kpinnoes <khorapin.pingadpu@mail.kmutt.ac.th>
2021-10-28 10:53:04 +0700

    [Add] Website

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    extend: {
+      colors: {
+        'graywhite': '#fafafa',
+
+      }
+
+    },
+  },
+  variants: {
+    extend: {
+      animation: ['hover', 'focus'],
+    },
+  },
+  plugins: [],
+}
